45f0fb9d16ba9563f3116d8ae72c629619871e71
jessiealbarian <jessie.albarian@alpin.io>
2017-09-17 15:06:33 -0600

    circles moving up and down

--- Assignment2/canvas.js
+++ Assignment2/canvas.js
@@ -14,7 +14,7 @@ function init() {
 var colors = [["#1962FF"], ["#1790E8"], ["#26DDFF"], ["#17E8D0"], ["#19FFA4"]];
 
 // Animates circles
-requestAnimationFrame(function loop(){
+requestAnimationFrame(function loop() {
     requestAnimationFrame(loop);
 
     // Refresh canvas
@@ -29,7 +29,7 @@ requestAnimationFrame(function loop(){
 });
 
 // Defines circle objects
-var Circle = function(color){
+var Circle = function (color) {
     // x and y coordinates
     this.x = Math.floor(Math.random() * (700) + 1);
     this.y = Math.floor(Math.random() * (500) + 1);
@@ -39,6 +39,8 @@ var Circle = function(color){
 
     // Colors
     this.c = color;
+
+    this.t = Math.random() * (50) + 1;
 };
 
 // Defines circle methods
@@ -46,7 +48,7 @@ Circle.prototype = {
     // constructor: Circle,
 
     // Draw circles
-    render: function(){
+    render: function () {
         mainContext.strokeStyle = this.c;
         mainContext.beginPath();
         mainContext.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
@@ -54,24 +56,31 @@ Circle.prototype = {
     },
 
     // Move circles to the left
-    move: function(){
-       this.x = this.x - 1;
-       // Moves circle from left to right of screen after it exits the screens
-       if (this.x + this.r === 0){
-           this.x = canvas.width + this.r;
-       }
+    move: function () {
+        // Move to the left
+        this.x = this.x - 1;
+
+        // Move up and down
+        this.y += (Math.sin(this.t/20))*1.2;
+
+        // Moves circle from left to right of screen after it exits the screens
+        if (this.x + this.r === 0) {
+            this.x = canvas.width + this.r;
+        }
+
+        this.t++;
     }
 };
 
 var circleList = [];
 
 // Create circle
-function createCircle(){
+function createCircle() {
     var circleObject = new Circle(colors[Math.floor(Math.random() * 5) + 1]);
     return circleObject;
 }
 
-for(var i = 0; i < 100; i++){
+for (var i = 0; i < 100; i++) {
     circleList.push(createCircle());
 }
 
