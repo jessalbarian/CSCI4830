cef63e13de8e928e7cef2f749aa5a5751ab22483
jessiealbarian <jessie.albarian@alpin.io>
2017-09-17 14:53:06 -0600

    Circles are now objects, and they are moving to the left

--- Assignment2/canvas.js
+++ Assignment2/canvas.js
@@ -1,8 +1,6 @@
-// window.addEventListener("load", function () {
-
 function init() {
     // Access main canvas
-    var canvas = document.getElementById("mainCanvas");
+    canvas = document.getElementById("mainCanvas");
 
     // Set canvas height and width
     canvas.width = 700;
@@ -10,20 +8,71 @@ function init() {
 
     // 2D drawing context
     mainContext = canvas.getContext("2d");
-
-    // Set main colors
-    colors = [["#1962FF"], ["#1790E8"], ["#26DDFF"], ["#17E8D0"], ["#19FFA4"]];
-
-    // Draw circles on canvas
-    drawCircles();
 }
 
-// Create 100 circles with random sizes and colors from my color palette
-function drawCircles() {
+// Global variables
+var colors = [["#1962FF"], ["#1790E8"], ["#26DDFF"], ["#17E8D0"], ["#19FFA4"]];
+
+// Animates circles
+requestAnimationFrame(function loop(){
+    requestAnimationFrame(loop);
+
+    // Refresh canvas
+    mainContext.fillStyle = "#ffffff";
+    mainContext.fillRect(0, 0, canvas.width, canvas.height);
+
     for (var i = 0; i < 100; i++) {
-        mainContext.strokeStyle = colors[Math.floor(Math.random() * 5) + 1];
-        mainContext.beginPath();
-        mainContext.arc(Math.floor(Math.random() * (700) + 1), Math.floor(Math.random() * (500) + 1), Math.floor(Math.random() * (20) + 1), 0, 2 * Math.PI);
-        mainContext.stroke();
+        var currentCircle = circleList[i];
+        currentCircle.move();
+        currentCircle.render();
     }
+});
+
+// Defines circle objects
+var Circle = function(color){
+    // x and y coordinates
+    this.x = Math.floor(Math.random() * (700) + 1);
+    this.y = Math.floor(Math.random() * (500) + 1);
+
+    // Radius
+    this.r = Math.floor(Math.random() * (20) + 1);
+
+    // Colors
+    this.c = color;
+};
+
+// Defines circle methods
+Circle.prototype = {
+    // constructor: Circle,
+
+    // Draw circles
+    render: function(){
+        mainContext.strokeStyle = this.c;
+        mainContext.beginPath();
+        mainContext.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        mainContext.stroke();
+    },
+
+    // Move circles to the left
+    move: function(){
+       this.x = this.x - 1;
+       // Moves circle from left to right of screen after it exits the screens
+       if (this.x + this.r === 0){
+           this.x = canvas.width + this.r;
+       }
+    }
+};
+
+var circleList = [];
+
+// Create circle
+function createCircle(){
+    var circleObject = new Circle(colors[Math.floor(Math.random() * 5) + 1]);
+    return circleObject;
 }
+
+for(var i = 0; i < 100; i++){
+    circleList.push(createCircle());
+}
+
+// TODO: up and down slightly
