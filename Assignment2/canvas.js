5b63e63835ab7167cfbd821d6aefd1e0ba070b5f
jessiealbarian <jessie.albarian@alpin.io>
2017-09-21 20:00:51 -0600

    working now

--- Assignment2/canvas.js
+++ Assignment2/canvas.js
@@ -5,6 +5,21 @@ var mouse = {x: 0, y: 0, ox: 0, oy: 0, vx: 0, vy: 0};
 var canvas = document.getElementById("mainCanvas");
 var dx = 0, dy = 0;
 var speed = 100; // px per second
+var fps = 0;
+var framesThisSecond = 0;
+var maxFps = 60;
+var frameCount = 0;
+var lastFrameTimeMs = 0;
+
+function updateFrameRate(newFrameRate) {
+    maxFps = newFrameRate;
+    document.getElementById("FrameRateLabel").innerHTML = maxFps;
+}
+
+setInterval(function () {
+    fps = framesThisSecond;
+    framesThisSecond = 0;
+}, 1000);
 
 // Initializes canvas
 function init() {
@@ -50,7 +65,7 @@ Circle.prototype = {
         this.x = this.x - 1;
 
         // Move up and down: inside sin = rate of moving up and down; outside sin = change in height
-        this.y += (Math.sin(this.t/20))*1.2;
+        this.y += (Math.sin(this.t / 20)) * 1.2;
 
         // Moves circle from left to right of screen after it exits the screens
         if (this.x + this.r <= 0) {
@@ -68,8 +83,8 @@ Circle.prototype = {
                 d = 1;
             }
             d = Math.sqrt(d);
-            this.x += mouse.vx/d;
-            this.y += mouse.vy/d;
+            this.x += mouse.vx / d;
+            this.y += mouse.vy / d;
         }
 
         this.t++;
@@ -115,25 +130,28 @@ Fish.prototype = {
         mainContext.ellipse(this.x - (90 * this.size), this.y + (20 * this.size), (30 * this.size), (45 * this.size), -8.4292, 0, 2 * Math.PI);
         mainContext.fillStyle = "orange";
         mainContext.fill();
+
+        ++framesThisSecond;
+        ++frameCount;
     },
 
     // Move fish along path
     move: function (x, y) {
 
-        if (x !== 0 && y !== 0){
+        if (x !== 0 && y !== 0) {
             // Move little fish
             this.x = x - 100;
-            this.y = y + Math.sin(this.x/10)*4 - 100;
+            this.y = y + Math.sin(this.x / 10) * 4 - 100;
         } else {
             // Move big fish
             this.x = this.x + 2;
-            this.y = this.y + Math.sin(this.x/20)*1.5;
+            this.y = this.y + Math.sin(this.x / 20) * 1.5;
         }
 
         // Moves fish from right to left of screen after it exits the screens
         if (this.x - 120 >= 900) {
             this.x = -75;
-            if (x !== 0 && y !== 0){
+            if (x !== 0 && y !== 0) {
                 // Reset y value for little fish
                 this.y = y - 100;
             } else {
@@ -144,7 +162,7 @@ Fish.prototype = {
 };
 
 // Add cursor interaction to bubbles
-canvas.addEventListener('mousemove', function(e){
+canvas.addEventListener('mousemove', function (e) {
     // Gets canvas info
     var rect = canvas.getBoundingClientRect();
 
@@ -162,21 +180,21 @@ canvas.addEventListener('mousemove', function(e){
 });
 
 // Add cursor interaction to bubbles
-canvas.addEventListener('mouseout', function(e){
+canvas.addEventListener('mouseout', function (e) {
     // Update velocity
     mouse.vx = 0;
     mouse.vy = 0;
 });
 
 // Add key down interaction to fish
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     if (e.keyCode === 40) {
         dy = 2;
     }
 });
 
 // Add key up interaction to fish
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     if (e.keyCode === 38) {
         dy = -2;
     }
@@ -197,8 +215,17 @@ var bigFish = new Fish(300, 300, 1);
 var smallFish = new Fish(200, 400, 0.5);
 
 // Animates circles
-requestAnimationFrame(function loop() {
-    requestAnimationFrame(loop);
+requestAnimationFrame(loop);
+
+function loop(timestamp) {
+
+    if (timestamp < lastFrameTimeMs + (1000 / maxFps)) {
+        requestAnimationFrame(loop);
+        return;
+    }
+
+    delta = timestamp - lastFrameTimeMs;
+    lastFrameTimeMs = timestamp;
 
     // Refresh canvas
     mainContext.fillStyle = "#29abe2";
@@ -216,4 +243,6 @@ requestAnimationFrame(function loop() {
         currentCircle.move();
         currentCircle.render();
     }
-});
+
+    requestAnimationFrame(loop);
+}
